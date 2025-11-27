881e27d2e95c0cbfece66292550847725095e720
dipl0nt <tresixfdv@gmail.com>
2025-11-28 00:53:48 +0100

    evaluate_ExpressionTree compiles

--- ExpressionTree.c
+++ ExpressionTree.c
@@ -378,10 +378,47 @@ OperandVec *evaluate_ExpressionTree(ExpressionTreeNode *tree){
 	int i = 0;
 	ExpressionTreeNode_ListNode *cur = tree->args.head;
 	for (int j=0;j<tree->args.count;j++){
-		
+		if (cur->treeNode->token.type==OPERAND){
+			result->values[i++] = ((Operand*)cur->treeNode->token.data)->value ;
+		}
+		else if (cur->treeNode->token.type==OPERATOR){
+			result->values[i++] = evaluate_ExpressionTree(cur->treeNode)->values[0] ; //remember that functions must always return exactely 1 value
+		}
+		else if (cur->treeNode->token.type==EXPRESSIONTREENODE_LIST){ //should be the only other case
+			result->values = (OPERAND_VALUE_TYPE*)realloc(result->values,(result->count+cur->treeNode->args.count)*sizeof(OPERAND_VALUE_TYPE));
+			//if (!result->values) ...
+			result->count = result->count + cur->treeNode->args.count ;
+			ExpressionTreeNode_ListNode *cur2 = cur->treeNode->args.head ;
+			for (int k=0;k<cur->treeNode->args.count;k++){
+				result->values[i++] = evaluate_ExpressionTree(cur2->treeNode)->values[0] ; //remember that functions must always return exactely 1 value
+				cur2 = cur2->next;
+			}
+		}
 
 		cur = cur->next;
 	}
+
+	if (tree->token.type==EXPRESSIONTREENODE_LIST){
+		return result;
+	}
+
+	if (tree->token.type==OPERATOR){ //should be the only remaning case
+		const Operator *op = (const Operator*)tree->token.data ;
+		if (result->count != op->arity ){
+			printf("ERROR while evaluating expression tree beacuse of arity mismatch for an operator \"\033[36m%s033[0m\"\n",op->symbol);
+			free_OperandVec(result);
+			return NULL;
+		}
+		OPERAND_VALUE_TYPE res = op->function(result->values) ;
+		free_OperandVec(result);
+		result = alloc_OperandVec(1);
+		result->values[0] = res ;
+
+		return result;
+	}
+
+	return NULL; //should never get here anyway
 }
+
 
 
